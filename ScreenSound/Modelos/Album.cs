772725fe823ea1ab3e253ec36f169c1948a61c6f
Lucas Garcia <lucasugarcia@gmail.com>
2026-04-05 20:46:02 -0300

    Implementação de interface IAvaliavel para avaliação de bandas, músicas e álbuns

--- ScreenSound/Modelos/Album.cs
+++ ScreenSound/Modelos/Album.cs
@@ -1,6 +1,6 @@
 ﻿namespace ScreenSound.Modelos;
 
-internal class Album
+internal class Album : IAvaliavel
 {
     private List<Musica> musicas = new List<Musica>();
 
@@ -12,11 +12,29 @@ internal class Album
     public string Nome { get; }
     public int DuracaoTotal => musicas.Sum(m => m.Duracao);
     public List<Musica> Musicas => musicas;
+    private List<Avaliacao> notas = new();
+
+    public double Media
+    {
+        get
+        {
+            if (notas.Count == 0)
+                return 0;
+            else
+                return notas.Average(a => a.Nota);
+        }
+    }
+
+    public void AdicionarNota(Avaliacao nota)
+    {
+        notas.Add(nota);
+    }
 
     public void AdicionarMusica(Musica musica)
     {
         musicas.Add(musica);
     }
+
 
     public void ExibirMusicasDoAlbum()
     {
